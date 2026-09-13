91499992ed47a62b7395418256c938ec3bae1e7d
mrrohanhiremath <rohanphiremath49@gmail.com>
2026-09-13 13:30:28 +0530

    day6

---
+++ Day6/Day6_Assignment_Testcase1.java
@@ -0,0 +1,154 @@
+package capgeminiAssignmentsOnSelenium.Day6;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Day6_Assignment_Testcase1 {
+	public static void main(String[] args) throws IOException, InterruptedException, AWTException {
+		ChromeOptions settings = new ChromeOptions();
+		Map<String, Object> prefs = new HashMap<>();
+		prefs.put("profile.password_manager_leak_detection", false);
+		settings.setExperimentalOption("prefs", prefs);
+		
+		FileInputStream fis = new FileInputStream("./src/test/java/capgeminiAssignmentsOnSelenium/Day6/day6commondata.properties");
+		Properties p = new Properties();
+		p.load(fis);
+		String BROWSER = p.getProperty("browser");
+		String URL = p.getProperty("url");
+		String USERNAME = p.getProperty("username");
+		String PASSWORD = p.getProperty("password");
+		
+		//excel
+		FileInputStream fis2 = new FileInputStream("./src/test/java/capgeminiAssignmentsOnSelenium/Day6/day6 (1).xlsx");
+		Workbook wb = WorkbookFactory.create(fis2);
+		String FIRSTNAME = wb.getSheet("sheet1").getRow(1).getCell(0).getStringCellValue();
+		String MIDDLENAME = wb.getSheet("sheet1").getRow(1).getCell(1).getStringCellValue();
+		String LASTNAME = wb.getSheet("sheet1").getRow(1).getCell(2).getStringCellValue();
+		//String EMPLOYEEID = wb.getSheet("sheet1").getRow(1).getCell(3).getStringCellValue();
+		String USERNAMEPIM = wb.getSheet("sheet1").getRow(1).getCell(4).getStringCellValue();
+		String PASSWORDPIM = wb.getSheet("sheet1").getRow(1).getCell(5).getStringCellValue();
+		String CPASSWORDPIM = wb.getSheet("sheet1").getRow(1).getCell(6).getStringCellValue();
+		String EMPLOYEENAME = wb.getSheet("sheet1").getRow(1).getCell(7).getStringCellValue();
+		
+		WebDriver driver=null;
+		
+		if(BROWSER.equals("chrome")) {
+			driver=new ChromeDriver(settings);
+		}
+		if(BROWSER.equals("edge")) {
+			driver=new EdgeDriver();
+		}
+		if(BROWSER.equals("Firefox")) {
+			driver=new FirefoxDriver();
+		}
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		//go to orangehrm application
+		driver.get(URL);
+		Thread.sleep(2000);
+		//username
+		driver.findElement(By.xpath("//input[@placeholder=\"Username\"]")).sendKeys(USERNAME);
+		Thread.sleep(2000);
+		//password
+		driver.findElement(By.xpath("//input[@placeholder=\"Password\"]")).sendKeys(PASSWORD);
+		Thread.sleep(2000);
+		//loginbutton
+		driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
+		Thread.sleep(2000);
+		//pim
+		driver.findElement(By.xpath("//span[text()='PIM']")).click();
+		Thread.sleep(2000);
+		//addbutton
+		driver.findElement(By.xpath("//button[@type=\"button\" and  @class=\"oxd-button oxd-button--medium oxd-button--secondary\"]")).click();
+		Thread.sleep(2000);
+		//firstname
+		driver.findElement(By.xpath("//input[@name=\"firstName\"]")).sendKeys(FIRSTNAME);
+		Thread.sleep(2000);
+		//middlename
+		driver.findElement(By.xpath("//input[@name=\"middleName\"]")).sendKeys(MIDDLENAME);
+		Thread.sleep(2000);
+		//lastname
+		driver.findElement(By.xpath("//input[@name=\"lastName\"]")).sendKeys(LASTNAME);
+		Thread.sleep(2000);
+//		//employeeid
+//		driver.findElement(By.xpath("")).sendKeys(EMPLOYEEID);
+		//create login toggle
+		driver.findElement(By.xpath("//span[@class=\"oxd-switch-input oxd-switch-input--active --label-right\"]")).click();
+		Thread.sleep(2000);
+		//username
+		driver.findElement(By.xpath("//label[text()='Username']/parent::div/following-sibling::div/child::input")).sendKeys(USERNAMEPIM);
+		Thread.sleep(2000);
+		//PASSWORD
+		driver.findElement(By.xpath("(//input[@type='password'])[1]")).sendKeys(PASSWORDPIM);
+		Thread.sleep(2000);
+		//confirmPassword
+		driver.findElement(By.xpath("(//input[@type='password'])[2]")).sendKeys(CPASSWORDPIM);
+		Thread.sleep(2000);
+		//click on save
+		driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
+		Thread.sleep(2000);
+		
+		
+		//admin link
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[normalize-space()=\"Admin\"]")).click();
+		//username
+		driver.findElement(By.xpath("(//input[@class=\"oxd-input oxd-input--active\"])[2]")).sendKeys(USERNAMEPIM);
+		Thread.sleep(2000);
+		//userrole
+		driver.findElement(By.xpath("(//i[@class=\"oxd-icon bi-caret-down-fill oxd-select-text--arrow\"])[1]")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//div[text()='ESS'])[1]")).click();
+		Thread.sleep(2000);
+		//employee name
+		driver.findElement(By.xpath("//input[@placeholder=\"Type for hints...\"]")).sendKeys(EMPLOYEENAME);
+		Thread.sleep(5000);
+		Robot r = new Robot();
+		r.keyPress(KeyEvent.VK_DOWN);
+		Thread.sleep(2000);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		Thread.sleep(2000);
+		r.keyPress(KeyEvent.VK_ENTER);
+		Thread.sleep(2000);
+		r.keyRelease(KeyEvent.VK_ENTER);
+		Thread.sleep(2000);
+		//status
+		driver.findElement(By.xpath("(//i[@class=\"oxd-icon bi-caret-down-fill oxd-select-text--arrow\"])[2]")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//div[text()='Enabled'])[1]")).click();
+		Thread.sleep(2000);
+		//submit
+		driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
+		Thread.sleep(2000);Thread.sleep(2000);
+		//verify
+		WebElement record = driver.findElement(By.xpath("//div[contains(text(),\"rohan hiremath\")]"));
+		if(record.isDisplayed()) {
+			System.out.println("Record is displayed");
+		}
+		else {
+			System.out.println("record is not displayed");
+		}
+		
+		Thread.sleep(5000);
+		driver.quit();
+	
+	}
+
+}
